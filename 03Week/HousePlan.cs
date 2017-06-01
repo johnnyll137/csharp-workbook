421eb5c717770580f4bb9363eff6379644ef8280
johnnyll137 <johnnyll137@gmail.com>
2017-06-01 18:17:48 -0500

    Updated HousePlan

--- 03Week/HousePlan.cs
+++ 03Week/HousePlan.cs
@@ -5,17 +5,20 @@ namespace _03Week
 
     public class Land
     {
-        public int house {get;set;}
+        public int house { get; set; }
+        public int tree { get; set; }
     }
     public class House
     {
         public int rooms { get; set; }
-        public int floors {get;set;}
+        public int floors { get; set; }
+        public int pool { get; set; }
     }
 
     public class Room
     {
         public string color { get; set; }
         public string floorsType { get; set; }
+        public int windows { get; set; }
     }
 }
--- 03Week/Program.cs
+++ 03Week/Program.cs
@@ -13,36 +13,51 @@ namespace _03Week
             while (countHouses <= land.house)
             {
 
-                House house = new House();
-
-                //Ask how many floors the house will have             
-                Console.WriteLine("How many floors do you want?");
-                house.floors = Convert.ToInt32(Console.ReadLine());
-                int countFloors = 1;
-                while (countFloors <= house.floors)
+                Console.WriteLine("How many trees do you want around house number {0}?", countHouses);
+                land.tree = Convert.ToInt32(Console.ReadLine());
                 {
-                    Console.WriteLine("How many rooms do you want on floor number {0}?", countFloors);
-                    house.rooms = Convert.ToInt32(Console.ReadLine());
 
-                    // We have to count how many rooms the house will have
-                    int countRooms = 1;
-                    while (countRooms <= house.rooms)
+                    House house = new House();
+
+                    Console.WriteLine("How many pools do you want?");
+                    house.pool = Convert.ToInt32(Console.ReadLine());
                     {
 
-                        Room room = new Room();
-                        Console.WriteLine("What color do you want room number {0} on floor {0} to be?", countRooms);
-                        room.color = Console.ReadLine();
+                        //Ask how many floors the house will have             
+                        Console.WriteLine("How many floors do you want?");
+                        house.floors = Convert.ToInt32(Console.ReadLine());
+                        int countFloors = 1;
+                        while (countFloors <= house.floors)
+                        {
+                            Console.WriteLine("How many rooms do you want on floor number {0}?", countFloors);
+                            house.rooms = Convert.ToInt32(Console.ReadLine());
 
-                        string response = string.Format("Your room color will be {0}", room.color);
-                        Console.WriteLine(response);
+                            // We have to count how many rooms the house will have
+                            int countRooms = 1;
+                            while (countRooms <= house.rooms)
+                            {
 
-                        countRooms++;
+                                Room room = new Room();
+
+                                Console.WriteLine("How many windows do you want in room {0} on floor {1} to have?", countRooms, countFloors);
+                                room.windows = Convert.ToInt32(Console.ReadLine());
+
+                                Console.WriteLine("What color do you want room number {0} on floor {1} to be?", countRooms, countFloors);
+                                room.color = Console.ReadLine();
+
+                                string response = string.Format("Your room color will be {0}", room.color);
+                                Console.WriteLine(response);
+
+                                countRooms++;
+                            }
+                            countFloors++;
+                        }
                     }
-                    countFloors++;
                 }
                 countHouses++;
             }
-            // Console.WriteLine("Hello World!");
         }
     }
 }
+// Console.WriteLine("Hello World!");
+
